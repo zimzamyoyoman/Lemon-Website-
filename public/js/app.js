fe7faefda052b4d018188eab4f1c1d582212e974
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-05-23 19:50:03 +0530

    Added 'lighting up of images on hover' effect

--- public/js/app.js
+++ public/js/app.js
@@ -6,4 +6,36 @@ $('.grid').isotope({
       columnWidth: 100
     }
   });
+
+//LEMON PICTURE LIGHT UP
+
+//NUMBER 1
+
+$(".grid-item-1").on("mouseover", function(){
+  $(".grid-lemons-2").attr("src", "../images/bright-lemon.jpg");
+});
+
+$(".grid-item-1").on("mouseleave", function(){
+  $(".grid-lemons-2").attr("src", "../images/dull-lemon.jpg");
+});  
+
   
+//NUMBER 2
+
+$(".grid-item-6").on("mouseover", function(){
+  $(".grid-lemons-7").attr("src", "../images/bright-lemon.jpg");
+});
+
+$(".grid-item-6").on("mouseleave", function(){
+  $(".grid-lemons-7").attr("src", "../images/dull-lemon.jpg");
+}); 
+
+//NUMBER 3
+
+$(".grid-item-8").on("mouseover", function(){
+  $(".grid-lemons-4").attr("src", "../images/bright-lemon.jpg");
+});
+
+$(".grid-item-8").on("mouseleave", function(){
+  $(".grid-lemons-4").attr("src", "../images/dull-lemon.jpg");
+}); 
