ced01256796ef1e97026850ef4b9f13b8e18f674
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-06-07 11:35:14 +0530

    Added scrolling effects for niche section

--- public/js/app.js
+++ public/js/app.js
@@ -48,29 +48,128 @@ $(".grid-item-8").on("mouseleave", function () {
   $(".grid-lemons-4").attr("src", "../images/dull-lemon.jpg");
 });
 
-//WORK SECTION JQUERY
+// PARALLAX SCROLLING
 
-// var $grid = $('.grid-w').isotope({
-//   itemSelector: '.grid-item-w',
-//   masonry: {
-//   }
+// var controller = new ScrollMagic.Controller();
+
+// $(function () { // wait for document ready
+//   // build scene
+//   var scene = new ScrollMagic.Scene({ triggerElement: "#pin2" })
+//     .setPin("#pin2")
+//     .addIndicators({ name: "2 (duration: 0)" }) // add indicators (requires plugin)
+//     .addTo(controller);
 // });
 
-// $(document).ready(function () {
-//   $(".grid-item-w").click(function () {
-//     alert("The grid item was clicked!");
-//     // create new item elements
-//     var $items = $('<div class="grid-item-w grid-item--width10-w grid-item--height10-w ad"> <button onclick="myFunction()">Try it</button> </div>');
-//     // append items to grid
-//     $grid.isotope('insert', $items)
-//   });
-// });
+//  SCROLLMAGIC FOR NICHE SECTION 
 
-// $grid.on( 'click', '.ad', function() {
-//   // remove clicked element
-//   $grid.isotope( 'remove', this )
-//     // layout remaining item elements
-//     .isotope('layout');
-// });
+$(document).ready(function () {
+
+  // Init ScrollMagic
+  var controller = new ScrollMagic.Controller();
+
+  var grid1 = new ScrollMagic.Scene({
+    triggerElement: '#niche-scroll',
+    triggerHook: 0.9
+  })
+    .setClassToggle('#grid1', 'fade-in')
+    .addIndicators({
+      name: 'grid1',
+      colorTrigger: 'black',
+      indent: 200,
+      colorStart: '#75C695'
+    })
+    .addTo(controller);
+
+  var grid2 = new ScrollMagic.Scene({
+    triggerElement: '#niche-scroll',
+    triggerHook: 0.7
+  })
+    .setClassToggle('#grid2', 'fade-in')
+    .addIndicators({
+      name: 'grid2',
+      colorTrigger: 'black',
+      indent: 200,
+      colorStart: '#75C695'
+    })
+    .addTo(controller);
+
+  var grid3 = new ScrollMagic.Scene({
+    triggerElement: '#niche-scroll',
+    triggerHook: 0.5
+  })
+    .setClassToggle('#grid3', 'fade-in')
+    .addIndicators({
+      name: 'grid3',
+      colorTrigger: 'black',
+      indent: 200,
+      colorStart: '#75C695'
+    })
+    .addTo(controller);
+
+  var grid4 = new ScrollMagic.Scene({
+    triggerElement: '#niche-scroll',
+    triggerHook: 0.3
+  })
+    .setClassToggle('#grid4', 'fade-in')
+    .addIndicators({
+      name: 'grid4',
+      colorTrigger: 'black',
+      indent: 200,
+      colorStart: '#75C695'
+    })
+    .addTo(controller);
+
+  var grid5 = new ScrollMagic.Scene({
+    triggerElement: '#grid5',
+    triggerHook: 0.9
+  })
+    .setClassToggle('#grid5', 'fade-in')
+    .addIndicators({
+      name: 'grid5',
+      colorTrigger: 'black',
+      indent: 200,
+      colorStart: '#75C695'
+    })
+    .addTo(controller);
+
+  var grid6 = new ScrollMagic.Scene({
+    triggerElement: '#grid5',
+    triggerHook: 0.7
+  })
+    .setClassToggle('#grid6', 'fade-in')
+    .addIndicators({
+      name: 'grid6',
+      colorTrigger: 'black',
+      indent: 200,
+      colorStart: '#75C695'
+    })
+    .addTo(controller);
+
+  var grid7 = new ScrollMagic.Scene({
+    triggerElement: '#grid4',
+    triggerHook: 0.5
+  })
+    .setClassToggle('#grid7', 'fade-in')
+    .addIndicators({
+      name: 'grid7',
+      colorTrigger: 'black',
+      indent: 200,
+      colorStart: '#75C695'
+    })
+    .addTo(controller);
 
 
+  var grid8 = new ScrollMagic.Scene({
+    triggerElement: '#grid4',
+    triggerHook: 0.3
+  })
+    .setClassToggle('#grid8', 'fade-in')
+    .addIndicators({
+      name: 'grid8',
+      colorTrigger: 'black',
+      indent: 200,
+      colorStart: '#75C695'
+    })
+    .addTo(controller);
+
+});
