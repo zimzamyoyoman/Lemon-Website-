1c334c6a130622ffe93aac4f78c5eeeef6cce1a0
Samarth Goyal <samarth_niku@yahoo.co.in>
2019-06-08 08:59:06 +0530

    Added scrolling animations for skill section

--- public/js/app.js
+++ public/js/app.js
@@ -60,12 +60,12 @@ $(document).ready(function () {
     triggerHook: 0.9
   })
     .setClassToggle('#grid1', 'fade-in')
-    .addIndicators({
-      name: 'grid1',
-      colorTrigger: 'black',
-      indent: 200,
-      colorStart: '#75C695'
-    })
+    // .addIndicators({
+    //   name: 'grid1',
+    //   colorTrigger: 'black',
+    //   indent: 200,
+    //   colorStart: '#75C695'
+    // })
     .addTo(controller);
 
   var grid2 = new ScrollMagic.Scene({
@@ -73,12 +73,12 @@ $(document).ready(function () {
     triggerHook: 0.7
   })
     .setClassToggle('#grid2', 'fade-in')
-    .addIndicators({
-      name: 'grid2',
-      colorTrigger: 'black',
-      indent: 200,
-      colorStart: '#75C695'
-    })
+    // .addIndicators({
+    //   name: 'grid2',
+    //   colorTrigger: 'black',
+    //   indent: 200,
+    //   colorStart: '#75C695'
+    // })
     .addTo(controller);
 
   var grid3 = new ScrollMagic.Scene({
@@ -86,12 +86,12 @@ $(document).ready(function () {
     triggerHook: 0.5
   })
     .setClassToggle('#grid3', 'fade-in')
-    .addIndicators({
-      name: 'grid3',
-      colorTrigger: 'black',
-      indent: 200,
-      colorStart: '#75C695'
-    })
+    // .addIndicators({
+    //   name: 'grid3',
+    //   colorTrigger: 'black',
+    //   indent: 200,
+    //   colorStart: '#75C695'
+    // })
     .addTo(controller);
 
   var grid4 = new ScrollMagic.Scene({
@@ -99,12 +99,12 @@ $(document).ready(function () {
     triggerHook: 0.3
   })
     .setClassToggle('#grid4', 'fade-in')
-    .addIndicators({
-      name: 'grid4',
-      colorTrigger: 'black',
-      indent: 200,
-      colorStart: '#75C695'
-    })
+    // .addIndicators({
+    //   name: 'grid4',
+    //   colorTrigger: 'black',
+    //   indent: 200,
+    //   colorStart: '#75C695'
+    // })
     .addTo(controller);
 
   var grid5 = new ScrollMagic.Scene({
@@ -112,12 +112,12 @@ $(document).ready(function () {
     triggerHook: 0.9
   })
     .setClassToggle('#grid5', 'fade-in')
-    .addIndicators({
-      name: 'grid5',
-      colorTrigger: 'black',
-      indent: 200,
-      colorStart: '#75C695'
-    })
+    // .addIndicators({
+    //   name: 'grid5',
+    //   colorTrigger: 'black',
+    //   indent: 200,
+    //   colorStart: '#75C695'
+    // })
     .addTo(controller);
 
   var grid6 = new ScrollMagic.Scene({
@@ -125,12 +125,12 @@ $(document).ready(function () {
     triggerHook: 0.7
   })
     .setClassToggle('#grid6', 'fade-in')
-    .addIndicators({
-      name: 'grid6',
-      colorTrigger: 'black',
-      indent: 200,
-      colorStart: '#75C695'
-    })
+    // .addIndicators({
+    //   name: 'grid6',
+    //   colorTrigger: 'black',
+    //   indent: 200,
+    //   colorStart: '#75C695'
+    // })
     .addTo(controller);
 
   var grid7 = new ScrollMagic.Scene({
@@ -138,12 +138,12 @@ $(document).ready(function () {
     triggerHook: 0.5
   })
     .setClassToggle('#grid7', 'fade-in')
-    .addIndicators({
-      name: 'grid7',
-      colorTrigger: 'black',
-      indent: 200,
-      colorStart: '#75C695'
-    })
+    // .addIndicators({
+    //   name: 'grid7',
+    //   colorTrigger: 'black',
+    //   indent: 200,
+    //   colorStart: '#75C695'
+    // })
     .addTo(controller);
 
 
@@ -152,12 +152,12 @@ $(document).ready(function () {
     triggerHook: 0.3,
   })
     .setClassToggle('#grid8', 'fade-in')
-    .addIndicators({
-      name: 'grid8',
-      colorTrigger: 'black',
-      indent: 200,
-      colorStart: '#75C695'
-    })
+    // .addIndicators({
+    //   name: 'grid8',
+    //   colorTrigger: 'black',
+    //   indent: 200,
+    //   colorStart: '#75C695'
+    // })
     .addTo(controller);
 
   var nicheBackground = new ScrollMagic.Scene({
@@ -165,43 +165,171 @@ $(document).ready(function () {
     triggerHook: 0.9
   })
     .setClassToggle('.niche', 'niche-background')
-    .addIndicators({
-      name: 'niche-background',
-      colorTrigger: 'black',
-      indent: 200,
-      colorStart: '#75C695'
-    })
+    // .addIndicators({
+    //   name: 'niche-background',
+    //   colorTrigger: 'black',
+    //   indent: 200,
+    //   colorStart: '#75C695'
+    // })
     .addTo(controller);
 
   var teamBackground = new ScrollMagic.Scene({
     triggerElement: '#team-trigger',
     triggerHook: 0.7
-    
+
   })
     .setClassToggle('.team', 'team-background')
+    // .addIndicators({
+    //   name: 'niche-background',
+    //   colorTrigger: 'black',
+    //   indent: 200,
+    //   colorStart: '#75C695'
+    // })
+    .addTo(controller);
+
+});
+
+// SCROLLMAGIC FOR SKILLS SECTION
+
+$(document).ready(function () {
+
+  // Init ScrollMagic
+  var controller2 = new ScrollMagic.Controller();
+
+  var skillBackground = new ScrollMagic.Scene({
+    triggerElement: '#skill-trigger',
+    triggerHook: 0.7,
+    duration: '150%'
+  })
+    .setClassToggle('.skill', 'skill-background')
+    // .addIndicators({
+    //   name: 'skill-background',
+    //   colorTrigger: 'black',
+    //   indent: 200,
+    //   colorStart: '#75C695'
+    // })
+    .addTo(controller2);
+
+  var gridS1 = new ScrollMagic.Scene({
+    triggerElement: '#skill-trigger',
+    triggerHook: 0.3
+  })
+    .setClassToggle('#skill-grid-1', 'skill-grid')
     .addIndicators({
-      name: 'niche-background',
+      name: 'skill-grid1',
       colorTrigger: 'black',
-      indent: 200,
+      indent: 500,
       colorStart: '#75C695'
     })
-    .addTo(controller);
+    .addTo(controller2);
+
+  var gridS2 = new ScrollMagic.Scene({
+    triggerElement: '#skill-trigger',
+    triggerHook: 0.25
+  })
+    .setClassToggle('#skill-grid-2', 'skill-grid')
+    .addIndicators({
+      name: 'skill-grid2',
+      colorTrigger: 'black',
+      indent: 500,
+      colorStart: '#75C695'
+    })
+    .addTo(controller2);
+
+  var gridS3 = new ScrollMagic.Scene({
+    triggerElement: '#skill-trigger',
+    triggerHook: 0.2
+  })
+    .setClassToggle('#skill-grid-3', 'skill-grid')
+    .addIndicators({
+      name: 'skill-grid3',
+      colorTrigger: 'black',
+      indent: 500,
+      colorStart: '#75C695'
+    })
+    .addTo(controller2);
+
+  var gridS4 = new ScrollMagic.Scene({
+    triggerElement: '#skill-trigger',
+    triggerHook: 0.15
+  })
+    .setClassToggle('#skill-grid-4', 'skill-grid-h')
+    .addIndicators({
+      name: 'skill-grid4',
+      colorTrigger: 'black',
+      indent: 500,
+      colorStart: '#75C695'
+    })
+    .addTo(controller2);
+
+  var gridS5 = new ScrollMagic.Scene({
+    triggerElement: '#skill-trigger',
+    triggerHook: 0.1
+  })
+    .setClassToggle('#skill-grid-5', 'skill-grid')
+    .addIndicators({
+      name: 'skill-grid5',
+      colorTrigger: 'black',
+      indent: 500,
+      colorStart: '#75C695'
+    })
+    .addTo(controller2);
+
+  var gridS6 = new ScrollMagic.Scene({
+    triggerElement: '#skill-trigger',
+    triggerHook: 0.05
+  })
+    .setClassToggle('#skill-grid-6', 'skill-grid')
+    .addIndicators({
+      name: 'skill-grid6',
+      colorTrigger: 'black',
+      indent: 500,
+      colorStart: '#75C695'
+    })
+    .addTo(controller2);
+
+  var gridS7 = new ScrollMagic.Scene({
+    triggerElement: '#skill-trigger',
+    triggerHook: 0
+  })
+    .setClassToggle('#skill-grid-7', 'skill-grid')
+    .addIndicators({
+      name: 'skill-grid7',
+      colorTrigger: 'black',
+      indent: 500,
+      colorStart: '#75C695'
+    })
+    .addTo(controller2);
+
+  var gridS8 = new ScrollMagic.Scene({
+    triggerElement: '#skill-trigger',
+    triggerHook: -0.05
+  })
+    .setClassToggle('#skill-grid-8', 'skill-grid-h')
+    .addIndicators({
+      name: 'skill-grid8',
+      colorTrigger: 'black',
+      indent: 500,
+      colorStart: '#75C695'
+    })
+    .addTo(controller2);
+
 
 });
 
 // FACTS SECTION CODE 
 
-$(document).ready(function(){
-  $("#next-arrow").click(function(){
+$(document).ready(function () {
+  $("#next-arrow").click(function () {
     $(".fact-lemon-imgs").animate({
-      left: '-=200px'
-    }, 600, 'swing');
+      left: '-=150px'
+    }, 500, 'swing');
   });
 });
-$(document).ready(function(){
-  $("#prev-arrow").click(function(){
+$(document).ready(function () {
+  $("#prev-arrow").click(function () {
     $(".fact-lemon-imgs").animate({
-      left: '+=200px'
+      left: '+=150px'
     }, 600, 'swing');
   });
 });
